987d36f5d44ec424930bc0a0d073bf267e17801b
Simon <sbrugel@udel.edu>
2023-10-09 13:07:30 -0400

    setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# UD CIS Scheduler - F23 CISC 275
+
+This is our final project for CISC 275
+
+## Setup
+- Clone the repository
+- Run `npm i` before you do anything else, this installs React and every other dependency
+
+You can use `npm run start` to launch the app locally.
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header">UD CIS Scheduler</header>
+            <p>
+                Edit <code>src/App.tsx</code> and save. This page will
+                automatically reload.
+            </p>
+        </div>
+    );
+}
+
+export default App;
