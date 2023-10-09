6c4203994c3c81ad223e3ab26df59229b59c2ad5
leochen <leochen@udel.edu>
2023-10-09 13:23:42 -0400

    Added Leo Chen

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App(): JSX.Element {
                 automatically reload.
             </p>
             <p>Simon Brugel was here</p>
+            <p>Leo Chen was here</p>
         </div>
     );
 }
