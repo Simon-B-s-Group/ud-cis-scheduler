eca206c1355e6c0e6323a97d8b3e9a265c654815
conorjur@udel.edu <65098646+ConorJurewicz@users.noreply.github.com>
2023-10-23 12:27:46 -0400

    small visual changes, including fonts, colors, favicon

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,12 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CIS Scheduler - F23 CISC 275
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-            <p>Simon Brugel was here</p>
-            <p>Cameron Wine was here</p>
-            <p>Conor Jurewicz was here</p>
-            <p>Leo Chen was here</p>
+            <header className="App-body">
+                <p>Simon Brugel was here</p>
+                <p>Cameron Wine was here</p>
+                <p>Conor Jurewicz was here</p>
+                <p>Leo Chen was here</p>
+            </header>
         </div>
     );
 }
