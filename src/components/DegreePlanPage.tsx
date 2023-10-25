bf97b83378d9bff46796b04b563a1a6a61829960
leochen <leochen@udel.edu>
2023-10-25 12:57:18 -0400

    this house is getting more and more furnished

--- src/components/DegreePlanPage.tsx
+++ src/components/DegreePlanPage.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 import { DegreePlan } from "../interfaces/degreePlan";
-import { Semester } from "../interfaces/semester";
 
 export function DegreePlanPage({ degreePlan }: { degreePlan: DegreePlan }) {
-    return (
-        <>
-            <h1>{degreePlan.name}</h1>
-            {degreePlan.semesters.map((sem: Semester): JSX.Element => {
-                return (
-                    <>
-                        <h3>
-                            COMPONENT FOR {sem.season} {sem.year} Semester GOES
-                            HERE!!!! Will need to call Semester component here
-                            after Leo makes it
-                        </h3>
-                    </>
-                );
-            })}
-        </>
-    );
+    return <>Words</>;
+    // return (
+    //     <>
+    //         <h1>{degreePlan.name}</h1>
+    //         {degreePlan.semesters.map((sem: Semester): JSX.Element => {
+    //             return (
+    //                 <>
+    //                     <h3>
+    //                         {sem.season} {sem.year}
+    //                         <PlannedCourses sem==></PlannedCourses>
+    //                     </h3>
+    //                 </>
+    //             );
+    //         })}
+    //     </>
+    // );
 }
