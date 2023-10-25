70b2a8b439ef1be5d7e5e840a513ce1abff2cb2e
leochen <leochen@udel.edu>
2023-10-25 08:46:05 -0400

    ESLint disable be gone

--- src/components/SemesterView.tsx
+++ src/components/SemesterView.tsx
@@ -1,11 +1,9 @@
-/* eslint-disable react/react-in-jsx-scope */
-import "react";
+import React from "react";
 import { Semester } from "../interfaces/semester";
 import { PlannedCourses } from "./PlannedCourses";
 
 export function SemesterView({ season, year, courses }: Semester): JSX.Element {
     return (
-        // eslint-disable-next-line react/react-in-jsx-scope
         <div>
             {season} {year} {" Courses:"}
             {courses.map(PlannedCourses)}
