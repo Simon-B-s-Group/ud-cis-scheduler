5b251e1833d850d5626f4b497ecfe26ad64a2829
conorjur@udel.edu <65098646+ConorJurewicz@users.noreply.github.com>
2023-10-28 15:23:21 -0400

    css changes for tables

--- src/components/SemesterView.tsx
+++ src/components/SemesterView.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Semester } from "../interfaces/semester";
 import { PlannedCourses } from "./PlannedCourses";
 import { Course } from "../interfaces/course";
+import "../Table.css";
 
 export function SemesterView({ season, year, courses }: Semester): JSX.Element {
     return (
         <div>
             {season} {year} {" Courses:"}
-            <table>
+            <table className="center">
                 <th>Course</th>
                 <th>Credits</th>
                 <tr>{courses.map(PlannedCourses)}</tr>
