4cebf3bd97b9fb7f23dc86fb6eeb321915091d6b
Simon <sbrugel@udel.edu>
2023-11-01 13:31:49 -0400

    started the first tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,53 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import App from "./App";
 
-test("renders the course name somewhere", () => {
-    render(<App />);
-    const linkElement = screen.getByText(/CISC275/i);
-    expect(linkElement).toBeInTheDocument();
+describe("UD CIS Scheduler tests", () => {
+    // this is like launching the app
+    beforeEach(() => render(<App />));
+
+    test("A popup appears on load, which shows info about the program.", () => {
+        // checks if "Welcome to the UD CIS Scheduler!" is displayed on the screen
+        expect(
+            screen.getByText(/Welcome to the UD CIS Scheduler!/i)
+        ).toBeInTheDocument();
+    });
+
+    test("You can close the intro modal.", async () => {
+        const closeButton = screen.getByText("Close");
+        fireEvent.click(closeButton);
+
+        // the modal takes a little bit to close
+        // this returns the moment it is done closing
+        await waitFor(() => {
+            expect(
+                screen.queryByText("Welcome to the UD CIS Scheduler!")
+            ).not.toBeInTheDocument();
+        });
+    });
+
+    test("You can re-open the intro modal.", async () => {
+        const closeButton = screen.getByText("Close");
+        fireEvent.click(closeButton);
+
+        await waitFor(() => {
+            expect(
+                screen.queryByText("Welcome to the UD CIS Scheduler!")
+            ).not.toBeInTheDocument();
+        });
+
+        const aboutButton = screen.getByText("About");
+        fireEvent.click(aboutButton);
+
+        expect(
+            screen.getByText(/Welcome to the UD CIS Scheduler!/i)
+        ).toBeInTheDocument();
+    });
+
+    test("There is a sample plan on the screen, named Sample Degree Plan.", () => {
+        const closeButton = screen.getByText("Close");
+        fireEvent.click(closeButton);
+
+        expect(screen.getByText(/Sample Degree Plan/i)).toBeInTheDocument();
+    });
 });
