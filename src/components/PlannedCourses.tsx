a1548f4dbb67b723e6f64a8f8d97419ebc46e09d
winecg <winecg@udel.edu>
2023-11-03 12:49:06 -0400

    added forms to edit course codes, names, and credits

--- src/components/PlannedCourses.tsx
+++ src/components/PlannedCourses.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable no-extra-parens */
 // THIS WAS APPROVED BY AROMANDO - UNSOLVABLE ESLINT AND PRETTIER CONFLICTS
 
-import React from "react";
+import React, { useState } from "react";
 import { Course } from "../interfaces/course";
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 
 /**
  * A single row of the courses table for the semester view
@@ -20,21 +20,70 @@ export function PlannedCourses({
     editMode: boolean;
     deleteCourse: (course: Course) => void;
 }): JSX.Element {
+    const [editing, setEditing] = useState<boolean>(false);
+    const [currentCourse, setCurrentCourse] = useState<Course>(course);
+
+    const updateCode = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setCurrentCourse({
+            ...currentCourse,
+            code: event.target.value
+        });
+    };
+    const updateName = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setCurrentCourse({
+            ...currentCourse,
+            name: event.target.value
+        });
+    };
+    const updateCredits = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ): void => {
+        setCurrentCourse({
+            ...currentCourse,
+            credits: parseInt(event.target.value)
+        });
+    };
+
     return (
         <tr>
             <td>
-                {course.code}: {course.name}
+                {currentCourse.code}: {currentCourse.name}
                 <br />
             </td>
-            <td> {course.credits}</td>
+            <td> {currentCourse.credits}</td>
             {editMode ? (
                 <td>
+                    <Button onClick={() => setEditing(!editing)}>
+                        Edit Course
+                    </Button>
                     <Button
                         className="btn btn-danger"
                         onClick={() => deleteCourse(course)}
                     >
                         Delete Course
                     </Button>
+                    {editing ? (
+                        <Form.Group controlId="editCourse">
+                            <Form.Label>Course Code</Form.Label>
+                            <Form.Control
+                                type="text"
+                                value={currentCourse.code}
+                                onChange={updateCode}
+                            ></Form.Control>
+                            <Form.Label>Course Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                value={currentCourse.name}
+                                onChange={updateName}
+                            ></Form.Control>
+                            <Form.Label>Course Credits</Form.Label>
+                            <Form.Control
+                                type="text"
+                                value={currentCourse.credits}
+                                onChange={updateCredits}
+                            ></Form.Control>
+                        </Form.Group>
+                    ) : null}
                 </td>
             ) : null}
         </tr>
